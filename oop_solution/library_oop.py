bd120c1389b66bf70c269c3b7b9596055fbb596c
mosuwat <mosuwat50@hotmail.com>
2025-11-11 00:00:56 +0700

    add and tested member class, also rewrite and retest some method in book class

--- oop_solution/library_oop.py
+++ oop_solution/library_oop.py
@@ -1,24 +1,52 @@
 # Library Management System - Object Oriented Style
 
 class Book:
-    def __init__(self, id, title, author, available_copies):
+    def __init__(self, id = '000', title = 'test', author = 'test', available_copies = 999):
         self.id = id
         self.title = title
         self.author = author
         self.total_copies = available_copies
         self.available_copies = available_copies
 
-    def display_info(self):
-        print(f"{self.title} by {self.author} - {self.available_copies} copies available")
+    def book_info(self):
+        return f"{self.title} by {self.author} - {self.available_copies} copies available"
     
     def take_book(self):
         if self.available_copies <= 0:
-            print("Error: No copies available!")
+            return False
         else:
             self.available_copies -= 1
+            return True
     
     def return_book(self):
         if self.available_copies >= self.total_copies:
-            print("Error: Return more than total copies!")
+            return False
         else:
             self.available_copies += 1
+            return True
+
+class Member:
+    def __init__(self, id = '111', name = 'member_test', email = 'member_test@gmail.com'):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.borrowed_books = []
+
+    def borrow_book(self, book):
+        if len(self.borrowed_books) >= 3:
+            return "Error: Member has reached borrowing limit!"
+        elif book.take_book():
+            self.borrowed_books.append(book)
+            return f"{self.name} borrowed '{book.title}'"
+        else:
+            return "Error: No copies available!"
+    
+    def return_borrowed_books(self, book):
+        if book in self.borrowed_books:
+            if book.return_book():
+                self.borrowed_books.remove(book)
+                return f"{self.name} returned '{book.title}'"
+            else:
+                return f"Error: Return more than total copies!"
+        else:
+            return "Error: This member hasn't borrowed this book!"
--- oop_solution/test_oop.py
+++ oop_solution/test_oop.py
@@ -3,19 +3,35 @@ import library_oop as lo
 # Test Code for Object Oriented Library System
 
 #TEST book class
-book1 = lo.Book(123, "Paruj in the wonderland", "Suwat", 20)
-book2 = lo.Book(456, "Piya and the seven drawf", "tatsu", 1)
+book1 = lo.Book('123', "Paruj in the wonderland", "Suwat", 20)
+book2 = lo.Book('456', "Piya and the seven drawf", "tatsu", 1)
+book3 = lo.Book('222', "Supaporn and the beast", "sutat", 5)
 
 #Test take and return book
-book1.display_info()
+print("=== TEST TAKE AND RETURN BOOK ===")
+print(book1.book_info())
 book1.take_book()
-book1.display_info()
+print(book1.book_info())
 book1.return_book()
-book1.display_info()
+print(book1.book_info())
+book1.return_book()
+print(book2.book_info())
+if not book2.take_book():
+    print("ERROR!")
+if not book2.take_book():
+    print("ERROR!")
+print(book2.book_info())
+print()
 
-#Test book error
-book1.return_book()
-book2.display_info()
-book2.take_book()
-book2.take_book()
-book2.display_info()
+#Test member class
+print("=== TEST MEMBER TAKE AND RETURN ===")
+member1 = lo.Member('007', 'Alice', 'Alice@gmail.com')
+print(member1.borrow_book(book1))
+print(member1.return_borrowed_books(book1))
+print(member1.borrow_book(book2))
+print(member1.borrow_book(book3))
+print(member1.borrow_book(book3))
+print(member1.borrow_book(book1))
+print(member1.return_borrowed_books(book3))
+print(member1.return_borrowed_books(book3))
+print(member1.return_borrowed_books(book3))
